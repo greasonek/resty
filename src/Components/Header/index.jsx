2e716b51af53672ff7501431e45584abb07074de
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-09-23 11:37:47 -0700

    changed all class components to functional components

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+// import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
+const Header = () => {
+  return (
+    <header>
         <h1>RESTy</h1>
-      </header>
-    );
-  }
+      </header>  )
 }
 
 export default Header;
+
+// import React from 'react';
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+// import React from 'react'
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
+const Results = (props) => {
+  return (
+    <section>
+    <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+  </section>
+    )
 }
 
 export default Results;
+
+
+// import React from 'react';
+
+// class Results extends React.Component {
+//   render() {
+//     return (
+//       <section>
+//       <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+//     </section>
+//     );
+//   }
+// }
+
+// export default Results;
